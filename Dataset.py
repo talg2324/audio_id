1926ca70c9ba1f3dac751f1c3cfd3b583d93e93c
talg2324 <tgrutman@yahoo.com>
2020-12-03 18:59:17 +0200

    no pickle

--- Dataset.py
+++ Dataset.py
@@ -14,28 +14,13 @@ def get_data():
     return data
 
 #SHAI
-def append_user():
+def register_new_user():
 
     xls = pd.ExcelFile('./data/Example_Dataset.xlsx')
     new_user = pd.read_csv('./data/talg.csv')
     # If new user not in xls -> make new sheet
-    # Sheet names should be anonymous-> use import uuid
 
 #SHAI
 def crop_data(signal, desired_length):
 
     return
-#SHAI
-def pickle_data():
-
-    data = get_data()
-
-    # Turn to pickle
-
-    return
-#SHAI
-def unpickle_data():
-
-    # Read data
-
-    return #data
