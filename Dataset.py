be9fa8e0e14a2a1bbb5a1c233894f1219e020384
unknown <shainosatzki@gmail.com>
2020-12-04 13:54:40 +0200

    registerd user and naive crop

--- Dataset.py
+++ Dataset.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np 
+import os.path
 
-def get_data():
-
+def get_data(path):
+    # input - path of excel file! 
+    # output - data as a dict of ('sheet_name':pd.Dataframe)
     data = {}
 
-    xls = pd.ExcelFile('./data/Example_Dataset.xlsx')
+    xls = pd.ExcelFile(path)
 
     for sheet_name in xls.sheet_names:
         sheet = xls.parse(sheet_name).dropna()
@@ -14,13 +16,44 @@ def get_data():
     return data
 
 #SHAI
-def register_new_user():
+def register_new_user(user_path,dataset_path):
+    # input  - user_path = csv file of the audio recording
+    #        - dataset_path = excel_file of the dataset
+    # output - datasetset_dict as a dict of ('user_name':pd.Dataframe)
 
-    xls = pd.ExcelFile('./data/Example_Dataset.xlsx')
-    new_user = pd.read_csv('./data/talg.csv')
+    dataset_dict = get_data(dataset_path)
+    new_user = pd.read_csv(user_path,header=None)
     # If new user not in xls -> make new sheet
+    name_of_user = os.path.splitext(os.path.split(user_path)[1])[0]
 
-#SHAI
-def crop_data(signal, desired_length):
+#names = 
+    if name_of_user not in list(dataset_dict.keys()):
+        dataset_dict[name_of_user] = new_user.dropna()
 
-    return
+    return dataset_dict
+
+def crop_data(dataset_path, time_interval):
+    # input - xlsx dataset file and time_interval that is wanted to crop
+    # output - dict of ('user_name':cropped Dataframe)
+
+    data = get_data(dataset_path)
+
+    f = 800                            #[Hz]
+    time_int = time_interval                      #[sec]
+    samples = time_int*f
+    for key, value in data.items():
+        rec_size = value.shape[0]      # record size
+        if rec_size < 20*f :
+            del data[key]
+        else :
+            samp2crop = round((rec_size - samples)/2)
+            index1 = list(range(samp2crop))  ; index2 = list(range(rec_size-samp2crop, rec_size)) ; 
+            index = index1 + index2
+            new_value = value.drop(index)
+            data[key] = new_value
+
+    return data
+
+    a = 8
+    y = 6
+    
